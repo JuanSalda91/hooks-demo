0aad60ad62dac1c21c410ef3afa6a278bee88efa
JuanSalda91 <juansalda9131@gmail.com>
2026-01-09 14:38:31 -0500

    imports added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-
+// imports //
+import { Routes, Route, Link } from "react-router-dom";
+import CounterPage from "./pages/CounterPage.jsx";
+import TimerPage from "./pages/TimerPage.jsx";
+import TodoPage from "./pages/TodoPage.jsx";
+import ThemePage from "./pages/ThemePage.jsx";
+import ReducerPage from "./pages/ReducerPage.jsx";
+import CustomHookPage from "./pages/CustomHookPage.jsx";
 
 function App() {
 
