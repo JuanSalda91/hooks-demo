98de1b3c7f466ce470cfd80341f37e54d39149eb
JuanSalda91 <juansalda9131@gmail.com>
2026-01-09 18:04:51 -0500

    main file updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import ReactDOM from "reacr-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App/>
+   <ThemeProvider>
+     <App/>
+    </ThemeProvider>
   </BrowserRouter>
 );
