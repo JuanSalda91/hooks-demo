bd0915ba3e9d0f3713501d7ae4513eb4e6435b5a
JuanSalda91 <juansalda9131@gmail.com>
2026-01-09 14:42:25 -0500

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,32 @@ import ReducerPage from "./pages/ReducerPage.jsx";
 import CustomHookPage from "./pages/CustomHookPage.jsx";
 
 function App() {
-
   return (
-    <>
+    <div className="app">
+      <header>
+        <h1>React Hooks Playground</h1>
+        <nav>
+          <Link to="/">Counter</Link>
+          <Link to="/timer">Timer</Link>
+          <Link to="/todos">Todos</Link>
+          <Link to="/theme">Theme</Link>
+          <Link to="/reducer">Reducer</Link>
+          <Link to="/custom">Custom Hook</Link>
+        </nav>
+      </header>
 
-    </>
-  )
+      <main>
+        <Routes>
+          <Route path="/" element={<CounterPage />} />
+          <Route path="/timer" element={<TimerPage />} />
+          <Route path="/todos" element={<TodoPage />} />
+          <Route path="/theme" element={<ThemePage />} />
+          <Route path="/reducer" element={<ReducerPage />} />
+          <Route path="/custom" element={<CustomHookPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App
