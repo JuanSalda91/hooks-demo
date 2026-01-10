7d8aed09b9ec4504aad0f42617e4e613b4a9e33f
JuanSalda91 <juansalda9131@gmail.com>
2026-01-10 00:40:05 -0500

    updated file

---
+++ src/hooks/CustomHookPage.jsx
@@ -0,0 +1,32 @@
+import { useLocalStorage } from "./useLocalStorage.js";
+
+export default function CustomHookPage() {
+  const [name, setName] = useLocalStorage("name", "");
+  const [color, setColor] = useLocalStorage("favorite-color", "blue");
+
+  return (
+    <section>
+      <h2>Custom hook | useLocalStorage</h2>
+
+      <label>
+        Name:
+        <input
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Favorite color:
+        <input
+          value={color}
+          onChange={e => setColor(e.target.value)}
+        />
+      </label>
+
+      <p>
+        Hello {name || "stranger"}, your favorite color is {color}.
+      </p>
+    </section>
+  );
+}
